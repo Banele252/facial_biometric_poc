9f7651c3e9c496b627f69354446eb7e2b0c5c034
banele252 <banele.mdluli25@gmail.com>
2026-07-26 11:37:22 +0200

    Added the required file changes

--- Backend/internal_backend/main.py
+++ Backend/internal_backend/main.py
@@ -2,11 +2,26 @@ import os
 from fastapi import FastAPI,HTTPException
 from starlette import status
 from id_validation import id_validation
+import json
 
 
 #validating the identity API
 
 id_val_object = id_validation(id='9801315140089')
+print(id_val_object.senaty_excutor())
+
+app = FastAPI()
+
+
+@app.get('/id_pre-check', status_code=status.HTTP_200_OK)
+def id_pre_check(id:str):
+    try:
+        id_new_object = id_validation(id=id)
+        response = id_new_object.senaty_excutor()
+        return json.dumps(response)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 
 
